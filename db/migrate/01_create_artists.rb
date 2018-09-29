bdb5aa3620beebb14b34c496600276730517b57c
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-29 09:55:06 +0000

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,20 @@
+require "pry"
+
+class CreateArtists < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+
+
+end
---
+++ db/migrate/03_add_favorite_food_to_artists.rb
@@ -0,0 +1,5 @@
+class AddFavoriteFoodToArtists2 < ActiveRecord::Migration
+  def change
+    add_column :artists, :favorite_food, :string
+  end
+end
