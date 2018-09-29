3ef44eb1c4e391acc4d15ec90cadb3acefadb8ae
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-29 09:54:47 +0000

    automatically backed up by learn

--- db/migrate/03_add_favorite_food_to_artists.rb
+++ db/migrate/03_add_favorite_food_to_artists.rb
@@ -1,4 +1,4 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration
+class AddFavoriteFoodToArtists2 < ActiveRecord::Migration
   def change
     add_column :artists, :favorite_food, :string
   end
