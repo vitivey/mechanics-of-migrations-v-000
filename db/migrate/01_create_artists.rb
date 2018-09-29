ab9a3fea155ecb5075cbff74eea33f7495958127
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-29 09:28:07 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -9,6 +9,10 @@ class CreateArtists < ActiveRecord::Migration
 
   def change
     create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
       binding.pry
     end
   end
